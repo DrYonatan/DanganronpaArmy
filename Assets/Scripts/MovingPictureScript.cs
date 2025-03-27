ed8921e9692c3cb87cd0c5c1c63bb1753a464fea
Peleg <pelegagin@gmail.com>
2025-03-27 19:46:46 +0200

    updated moving image

--- Assets/Scripts/MovingPictureScript.cs
+++ Assets/Scripts/MovingPictureScript.cs
@@ -32,17 +32,17 @@ public class MovingPictureScript : MonoBehaviour
 
         GameObject image = GameObject.Find($"VN controller/Root/Canvas - Main/LAYERS/3 - Cinematic/MovingImage");
    
-        Vector3 left = new Vector3(-440, 0, 0);
+        Vector3 left = new Vector3(-0.3f, 0, 0);
         Vector3 startPos = image.transform.position;
         Vector3 leftTargetPos = startPos + left; // Adjust this vector to change the direction
         while (elapsedTime < duration)
         {
-            if (elapsedTime < 3f)
+            if (elapsedTime < 2f)
             {
                 image.transform.position = Vector3.Lerp(startPos, leftTargetPos, elapsedTime / 3f);
             }
 
-            else if (elapsedTime > 4f)
+            else if (elapsedTime > 3f)
             {
                 image.transform.position = Vector3.Lerp(leftTargetPos, startPos, elapsedTime2 / 3f);
                 elapsedTime2 += Time.deltaTime;
