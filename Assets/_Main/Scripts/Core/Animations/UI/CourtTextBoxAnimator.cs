b5aaa3faeacc539e7c65f67f3d68e4101c3931eb
DrYonatan <talagin11@gmail.com>
2025-07-23 17:15:13 +0300

    add face white flashing and proper mask

--- Assets/_Main/Scripts/Core/Animations/UI/CourtTextBoxAnimator.cs
+++ Assets/_Main/Scripts/Core/Animations/UI/CourtTextBoxAnimator.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using DG.Tweening;
 
 public class CourtTextBoxAnimator : TextBoxAnimations
 {
-    public CharacterFaceController characterFace; // only appears in court
+    public CharacterFaceController characterFace;
+    public Image faceWhiteOverlay;
+    public float faceFlashDuration;
 
     public override void TextBoxAppear()
     {
@@ -26,7 +29,28 @@ public class CourtTextBoxAnimator : TextBoxAnimations
 
     public void ChangeFace(string characterName)
     {
-        characterFace.SetFace(characterName);
+        Sequence seq = DOTween.Sequence();
+
+        // Ensure white overlay is enabled and fully transparent
+        faceWhiteOverlay.color = new Color(1, 1, 1, 0);
+        faceWhiteOverlay.gameObject.SetActive(true);
+
+        // Fade white overlay in
+        seq.Append(faceWhiteOverlay.DOFade(1f, faceFlashDuration));
+
+        // After flash, swap sprite
+        seq.AppendCallback(() => {
+            characterFace.SetFace(characterName);
+        });
+
+        // Fade white overlay out
+        seq.Append(faceWhiteOverlay.DOFade(0f, faceFlashDuration));
+
+        // Optional: hide overlay after
+        seq.AppendCallback(() => {
+            faceWhiteOverlay.gameObject.SetActive(false);
+        });
+        
     }
 
 
