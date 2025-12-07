4780ba40ff533c15989dd87c97b5460470287408
DrYonatan <talagin11@gmail.com>
2025-12-07 19:09:37 +0200

    add camera offsets

--- Assets/_Main/Scripts/Core/Trial Minigames/Logic Shoot/LogicShootManager.cs
+++ Assets/_Main/Scripts/Core/Trial Minigames/Logic Shoot/LogicShootManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -11,8 +12,10 @@ public class LogicShootManager : MonoBehaviour
     public LogicShootUIAnimator animator;
 
     public LogicShootSegment segment;
-    
+
     public GraphicRaycaster raycaster;
+    
+    public AnimationCurve cameraCurve;
 
     public float enemyHP;
     public float ammo;
@@ -36,6 +39,7 @@ public class LogicShootManager : MonoBehaviour
             StartCoroutine(CoolDown());
         }
     }
+
     private void ProcessShot()
     {
         PointerEventData pointerData = new PointerEventData(EventSystem.current);
@@ -49,7 +53,7 @@ public class LogicShootManager : MonoBehaviour
             var go = r.gameObject;
 
             RectTransform hole = CreateHole(go);
-            
+
             ShootTargetArea area = go.GetComponent<ShootTargetArea>();
             if (area != null)
             {
@@ -71,10 +75,10 @@ public class LogicShootManager : MonoBehaviour
             CameraController.instance.camera,
             out localPoint
         );
-            
+
         RectTransform hole = Instantiate(animator.holePrefab, go.transform);
         hole.anchoredPosition = localPoint;
-        
+
         return hole;
     }
 
@@ -101,7 +105,7 @@ public class LogicShootManager : MonoBehaviour
         CharacterCutIn cutIn = Instantiate(segment.cutIn, TrialManager.instance.globalUI);
         cutIn.Animate();
         yield return new WaitForSeconds(3f);
-        
+
         ImageScript.instance.FadeToBlack(0.2f);
         yield return CameraController.instance.DiscussionOutroMovement(2.5f);
         ImageScript.instance.UnFadeToBlack(0.2f);
@@ -109,18 +113,19 @@ public class LogicShootManager : MonoBehaviour
 
         CharacterStand characterStand =
             TrialManager.instance.characterStands.Find(stand => stand.character == segment.character);
-        
-        yield return CameraController.instance.SpinToTarget(characterStand.transform, characterStand.heightPivot, Vector3.zero,
+
+        yield return CameraController.instance.SpinToTarget(characterStand.transform, characterStand.heightPivot,
+            Vector3.zero,
             Vector3.zero, 0f);
-        
+
         animator.PlayStartAnimation();
         yield return CameraController.instance.MoveAndRotate(new Vector3(0, 0, 1f), Vector3.zero, 2.5f);
-        
+
         TrialManager.instance.FadeCharactersExcept(segment.character, 0f);
         animator.gameObject.SetActive(true);
 
         yield return new WaitForSeconds(0.5f);
-        
+
         TrialCursorManager.instance.Show();
         MusicManager.instance.PlaySong(animator.music);
 
@@ -141,12 +146,12 @@ public class LogicShootManager : MonoBehaviour
         segment.Finish();
         TrialManager.instance.FadeCharactersExcept(segment.character, 1f);
     }
-    
+
     public void DamageEnemy(float amount)
     {
         enemyHP -= amount;
         TrialManager.instance.barsAnimator.DecreaseHealthFromMeter(animator.enemyHpBar, amount, 0.2f);
-        
+
         if (enemyHP <= 0)
         {
             FinishGame();
@@ -155,7 +160,11 @@ public class LogicShootManager : MonoBehaviour
 
     private void MoveCameraToCenter(Vector3 from)
     {
-        CameraController.instance.cameraTransform.localPosition = from;
-        StartCoroutine(CameraController.instance.MoveAndRotate(Vector3.zero, Vector3.zero, 1f));
+        Vector3 originalPosition = CameraController.instance.cameraTransform.localPosition;
+
+        CameraController.instance.cameraTransform.localPosition += from;
+
+        CameraController.instance.cameraTransform
+            .DOLocalMove(originalPosition, 3f).SetEase(cameraCurve);
     }
 }
