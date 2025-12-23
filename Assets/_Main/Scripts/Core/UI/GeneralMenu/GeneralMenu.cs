e99ee5b8a091b171f6b0e694189415cf50e07a80
Peleg <pelegagin@gmail.com>
2025-12-23 21:47:19 +0200

    finish up general menu

---
+++ Assets/_Main/Scripts/Core/UI/GeneralMenu/GeneralMenu.cs
@@ -0,0 +1,123 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using DIALOGUE;
+using UnityEngine;
+
+public class GeneralMenu : MonoBehaviour
+{
+    public List<MenuButton> menuItems;
+    public int currentItemIndex;
+    public int columns = 3;
+    public AudioClip menuMove;
+    public AudioClip menuSelect;
+    public AudioClip menuFail;
+    public LogoAnimation logoAnimator;
+    public GameObject content;
+    public GameObject logo;
+    public Tween showContentTween;
+    public RectTransform mainPart;
+    public CanvasGroup mainPartCanvasGroup;
+    private float originalPosY;
+    private bool isInputActive;
+    
+    void Start()
+    {
+        originalPosY = mainPart.anchoredPosition.y;
+    }
+
+    void Update()
+    {
+        if (isInputActive)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                if (currentItemIndex > 0)
+                    currentItemIndex--;
+
+                UpdateCurrentItem();
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                if (currentItemIndex < menuItems.Count - 1)
+                    currentItemIndex++;
+                UpdateCurrentItem();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                if (currentItemIndex < menuItems.Count - columns)
+                    currentItemIndex += columns;
+                UpdateCurrentItem();
+            }
+            else if (Input.GetKeyDown(KeyCode.W))
+            {
+                if (currentItemIndex - columns >= 0)
+                    currentItemIndex -= columns;
+                UpdateCurrentItem();
+            }
+            else if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (menuItems[currentItemIndex].disabled)
+                {
+                    SoundManager.instance.PlaySoundEffect(menuFail);
+                }
+                else
+                {
+                    SoundManager.instance.PlaySoundEffect(menuSelect);
+                    menuItems[currentItemIndex].Click();
+                }
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PlayerInputManager.instance.TogglePause();
+        }
+    }
+
+    public void UpdateCurrentItem()
+    {
+        foreach (MenuButton menuButton in menuItems)
+        {
+            menuButton.StopHover();
+        }
+
+        SoundManager.instance.PlaySoundEffect(menuMove);
+
+        if (!menuItems[currentItemIndex].disabled)
+            menuItems[currentItemIndex].StartHover();
+    }
+
+
+    public void CloseMenu()
+    {
+        gameObject.SetActive(false);
+        showContentTween?.Kill();
+        logoAnimator.KillAll();
+    }
+
+    public void OpenMenu()
+    {
+        gameObject.SetActive(true);
+        content.SetActive(false);
+        isInputActive = false;
+        logo.SetActive(true);
+        logoAnimator.PlayAnimation();
+        menuItems[currentItemIndex].StopHover();
+        mainPartCanvasGroup.alpha = 0.4f;
+        mainPart.DOAnchorPosY(originalPosY - 200, 0f).SetUpdate(true);
+
+        showContentTween = DOVirtual.DelayedCall(Random.Range(1.5f, 4f), () =>
+        {
+            content.SetActive(true);
+            logo.SetActive(false);
+            currentItemIndex = 0;
+            mainPartCanvasGroup.DOFade(1f, 0.8f).SetUpdate(true);
+            mainPart.DOAnchorPosY(originalPosY, 0.8f).SetUpdate(true).OnComplete(() =>
+            {
+                if (!menuItems[currentItemIndex].disabled)
+                    menuItems[currentItemIndex].StartHover();
+                isInputActive = true;
+            });
+        });
+    }
+}
--- Assets/_Main/Scripts/Core/UI/PauseMenu/MenuButton.cs
+++ Assets/_Main/Scripts/Core/UI/PauseMenu/MenuButton.cs
@@ -1,28 +1,42 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour
 {
     public MenuScreen menuToOpen;
-    private bool isHovered = false;
     public bool disabled;
-    public Image buttonTexture;
     public MenuScreenContainer menuScreenContainer;
+    public RectTransform rectTransform;
+    public float originalPosY;
+    public Image blueAura;
+    public Image icon;
 
-    void Update()
+    void Start()
     {
-        buttonTexture.color = isHovered && !disabled ? Color.red : Color.clear;
+        originalPosY = rectTransform.anchoredPosition.y;
+        if (disabled)
+        {
+            icon.DOFade(0.3f, 0f).SetUpdate(true);
+        }
+
+        blueAura.DOFade(0f, 0f).SetUpdate(true);
     }
+
     public virtual void Click()
     {
-        if (!disabled)
-        {
-            menuScreenContainer.OpenMenu(menuToOpen);
-        }
+        menuScreenContainer.OpenMenu(menuToOpen);
     }
 
-    public void SetIsHovered(bool isHovered)
+    public void StartHover()
     {
-        this.isHovered = isHovered;
+        rectTransform.DOAnchorPosY(originalPosY + 40f, 0.2f).SetUpdate(true);
+        blueAura.DOFade(1f, 0.2f).SetUpdate(true);
+    }
+
+    public void StopHover()
+    {
+        rectTransform.DOAnchorPosY(originalPosY, 0.2f).SetUpdate(true);
+        blueAura.DOFade(0f, 0.2f).SetUpdate(true);
     }
 }
